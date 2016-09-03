82e132bc7c9033794181b04acd280692a3c48923
Juan Carlos Valerio Barreto <jc.valerio.b@gmail.com>
2016-09-02 21:11:36 -0500

    Consumiendo de API de la NASA https://api.nasa.gov/api.html

---
+++ batch11/angular/nasa/factory/factory.js
@@ -0,0 +1,13 @@
+(function(){
+	'use strict';
+
+	angular
+		.module("nasa")
+		.factory('apiNasa', apiNasa);
+
+	apiNasa.$inject = ["$resource"];
+	function apiNasa($resource) {
+		
+		return $resource ("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=3yyIu5h6y7pd055av9ZQUsbeGl3V8mr7JvK1idKc");
+	}
+})();
